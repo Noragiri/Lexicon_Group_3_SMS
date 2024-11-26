6047268769520e020574c717e97c3a084c872559
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2024-11-26 10:00:00 +0100

    24: Make modules out of some parts

--- hej/social_app/urls.py
+++ hej/social_app/urls.py
@@ -1,11 +1,21 @@
 """ URL Configuration for social_app app. """
 
 from django.urls import path
-from social_app.views import user_profile
+from social_app.views import (
+    user_profile,
+    search,
+    followers,
+    following,
+    temporary_startpage,
+)
 
 app_name = "social_app"
 
 urlpatterns = [
-    path("user_profile/", user_profile, name="user_profile_no_id"),
+    path("", temporary_startpage, name="home"),
+    path("user_profile/", user_profile, name="user_profile_my_profile"),
     path("user_profile/<int:user_id>/", user_profile, name="user_profile"),
+    path("followers/", followers, name="followers"),
+    path("following/", following, name="following"),
+    path("search/", search, name="search"),
 ]
