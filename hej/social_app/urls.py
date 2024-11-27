666ba09dc63b3a155aaa4299a402fc0d6abdf10b
Vaishali <jawale.vaishali@gmail.com>
2024-11-27 13:12:03 +0100

    17-search-followers

--- hej/social_app/urls.py
+++ hej/social_app/urls.py
@@ -1,11 +1,29 @@
 """ URL Configuration for social_app app. """
 
 from django.urls import path
-from social_app.views import user_profile
+from social_app.views import user_profile,user_profile_no_id,search_user,temp_profile
+from . import views
 
 app_name = "social_app"
 
+# urlpatterns = [
+#     path("user_profile/", user_profile, name="user_profile_no_id"),
+#     path("user_profile/<int:user_id>/", user_profile, name="user_profile"),
+#     path("", serach_user, name="serach_user"),
+#     path("temp/", temp_profile, name="temp_profile"),
+
+
+# ]
+
 urlpatterns = [
-    path("user_profile/", user_profile, name="user_profile_no_id"),
+    #path("user_profile/", views.user_profile, name="user_profile_no_id"),
+    path("", views.user_profile_no_id, name="user_profile_no_id"),
+
     path("user_profile/<int:user_id>/", user_profile, name="user_profile"),
+    path("search/", views.search_user, name="search_user"),
+    path("search/temp/", views.temp_profile, name="temp_profile"),
+
+
 ]
+
+
--- hej/social_app/views.py
+++ hej/social_app/views.py
@@ -1,6 +1,9 @@
 """ Views for the social app. """
 
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
+from django.db.models import Q   
+from django.contrib.auth.models import User
+
 
 
 app_name = "social_app"
@@ -21,10 +24,11 @@ def user_profile(request, user_id=0):
         "email": "hej@hej.com",
     }
 
+   
     return render(request, "social-app/user_profile.html", context)
 
 
-def user_profile_no_id(request, user_id=0):
+def user_profile_no_id(request,user_id=0):
     """Render the user profile."""
     """ Make database request for user with id passed in """
     context = {
@@ -37,3 +41,42 @@ def user_profile_no_id(request, user_id=0):
     }
 
     return render(request, "social-app/user_profile.html", context)
+
+
+#view to search user 
+
+def search_user(request):
+      if 'SearchQuery' in request.GET:
+       # get all users
+        #users=User.object.all()
+        data=None
+
+
+        search_query=request.GET.get('SearchQuery')
+
+        #using Query tool to add multiple quries to get data from model 
+        # based on first name or last name enetered in search box.
+        multiple_q=Q(Q(first_name__icontains=search_query) | Q(username__icontains=search_query)| Q(last_name__icontains=search_query))
+        data=User.objects.filter(multiple_q)
+    
+        context={'data':data}
+        print('data',data)
+
+
+        return render(request, "social-app/search.html", context)
+      else:
+            return render(request, "social-app/user_profile.html"   )
+               #return HttpResponse("No search query provided.")
+
+      return HttpResponse("Invalid request method.")
+
+#view to search user 
+
+def temp_profile(request):
+    temp_profile=User.objects.get(pk=1)
+    print('temp_profile')
+    context={'temp_profile':temp_profile}
+    return render(request, template_name='social-app/temp_profile.html',context=context)
+
+
+  
