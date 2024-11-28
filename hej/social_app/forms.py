ef229d22b4a14cdd4689a6f741f4de855c0c4e29
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2024-11-28 10:50:24 +0100

    30: stuff clean

--- hej/social_app/forms.py
+++ hej/social_app/forms.py
@@ -4,15 +4,30 @@ from social_app.models import UserProfile
 
 
 class UserForm(forms.ModelForm):
+    """Form for the User model"""
+
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
+        """Specify the model and fields to include in the form"""
+
         model = User
         fields = ("username", "email", "password")
         """TODO: Check if password == repeat password"""
 
 
 class UserProfileInfoForm(forms.ModelForm):
+    """Form for the UserProfile model"""
+
     class Meta:
+        """Specify the model and fields to include in the form"""
+
         model = UserProfile
         fields = ("bio",)
+
+    def clean_email(self):
+        """Validate that the email is unique"""
+        email = self.cleaned_data.get("email")
+        if UserProfile.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with that email already exists.")
+        return email
