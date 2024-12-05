932c2ce95d99f4513fa24a22e58f29b4e9d8c337
Noragiri <33696866+Noragiri@users.noreply.github.com>
2024-12-05 18:37:35 +0100

    my final message, goodbye

--- hej/social_app/admin.py
+++ hej/social_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from social_app.models import UserProfile, Post, UserFollow
+from social_app.models import UserProfile, Post, UserFollow, Comment
 
 # Register your models here.
 
 admin.site.register(UserProfile)
 admin.site.register(UserFollow)
 admin.site.register(Post)
+admin.site.register(Comment)
